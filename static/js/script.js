44c88fb1770c1c89141a193ebe1ef3f3b8347984
Vitor Mourão <vitormourao.dev@outlook.com>
2025-02-19 18:29:15 -0300

    add empyt button generation

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,3 @@
-/*function AddButton(){
-    const deckButton = document.createElement("div");
-    
-}
-
-function ClickButton(){
-    return;
-}
-
-document.querySelectorAll(".deckButton").forEach(div => {
-    div.addEventListener("click", function() {
-        console.log({div: this.dataset.action});
-    });
-});
-*/
 async function loadButtons(){
     let response = await fetch("/items");
     let buttons = await response.json();
@@ -21,17 +6,31 @@ async function loadButtons(){
 
     container.innerHTML = "";
 
+    let totalButtons = buttons.length;
+    let emptyButtonsNeeded = 10 - totalButtons;
+
     buttons.forEach(button => {
         let div = document.createElement("div");
+        
         div.classList.add("deckButton");
-        //div.id = `btns_${button.id}`;
-        //div.dataset.action = button.effect_type;
+        div.id = `btns_${button.id}`;
+        div.dataset.action = button.effect_type;
         div.textContent = button.name;
-
         container.append(div);
+
+       
+        div.addEventListener("click", function(){
+            console.log(this.dataset.action)
+        })
     })
 
+    for(let i = 0; i < emptyButtonsNeeded; i++){
+        let emptyButton = document.createElement("div");
 
+        emptyButton.classList.add("deckButton", "emptyButton");
+        emptyButton.textContent = "vazio";
+        container.append(emptyButton);
+    }
 }
 
 loadButtons();
