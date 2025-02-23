85fd691c4789d358c016da995cfa051a8c27cf17
Vitor Mourão <vitormourao.dev@outlook.com>
2025-02-22 21:02:21 -0300

    pylint warnings reduced

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ create_db_and_tables()
 
 app.register_blueprint(button_bp)
 
+"connection test route"
 @app.route("/")
 def hello_world():
     return "Hello World!"
 
+"deck route where we have the buttons"
 @app.route("/deck")
 def deck():
     return render_template("deck/deck.html")
